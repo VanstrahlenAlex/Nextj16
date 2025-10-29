cfadbd6c204b87b3668e65b8d304aa09308cd4cf
VanstrahlenAlex <vanstrahlenalex@gmail.com>
2025-10-29 18:14:02 -0500

    29/10/2025

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AboutPage = () => {
+	throw new Error("Error from About Page");
+  return (
+	  <div>AboutPage</div>
+  )
+}
+
+export default AboutPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Hello from '../components/hello';
+import Link from 'next/link';
 
 const Home = () => {
 	console.log("What type of a component am I?");
 	
-  return (
-	<div className='text-5xl underline'>Welcome to Next.js 16 !</div>
-  )
+	return (
+		<main>
+			<div className='text-5xl underline'>Welcome to Next.js 16 !</div>
+			<Hello />
+			<Link href="/dashboard/users">Go to Users</Link>
+		</main>
+	)
 }
 
 export default Home
